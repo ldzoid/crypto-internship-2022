5a4b41aaa7e2a17b5aadeb10fe27f730e62d764c
ldzoid <lucijandrnasin1@gmail.com>
2022-07-21 21:51:00 +0200

    replace deploy fixture repetition with beforeEach() inside each describe block

--- test/BlankHoodie.js
+++ test/BlankHoodie.js
@@ -1,115 +1,118 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("BlankHoodie contract", () => {
-  // fixture for basic ready-to-use contract environment
-  const deployContractFixture = async () => {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
-    const hhBlankHoodie = await BlankHoodie.deploy();
-
-    await hhBlankHoodie.setPaused(false);
-    await hhBlankHoodie.deployed();
-
-    return { BlankHoodie, hhBlankHoodie, owner, addr1, addr2 };
-  };
-
   describe("mint() functionality", () => {
-    it("user should be able to mint with enough ether", async () => {
-      const { owner, addr1, hhBlankHoodie } = await loadFixture(
-        deployContractFixture
-      );
+    let owner, user;
+    let hhBlankHoodie = null;
 
+    beforeEach(async () => {
+      [owner, user] = await ethers.getSigners();
+      const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
+      hhBlankHoodie = await BlankHoodie.deploy();
+
+      await hhBlankHoodie.setPaused(false);
+      await hhBlankHoodie.deployed();
+    });
+
+    it("user should be able to mint with enough ether", async () => {
       // increase in balance after mint
       await hhBlankHoodie
-        .connect(addr1)
+        .connect(user)
         .mint(1, { value: ethers.utils.parseEther("0.1") });
-      expect(await hhBlankHoodie.balanceOf(addr1.address)).to.equal(1);
+      expect(await hhBlankHoodie.balanceOf(user.address)).to.equal(1);
 
       await hhBlankHoodie.mint(3, { value: ethers.utils.parseEther("0.3") });
       expect(await hhBlankHoodie.balanceOf(owner.address)).to.equal(3);
 
       // token IDs should match in order
-      expect(await hhBlankHoodie.ownerOf(0)).to.equal(addr1.address);
+      expect(await hhBlankHoodie.ownerOf(0)).to.equal(user.address);
       expect(await hhBlankHoodie.ownerOf(1)).to.equal(owner.address);
       expect(await hhBlankHoodie.ownerOf(3)).to.equal(owner.address);
     });
 
     it("should revert if user tries to mint without enough ether", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
-
       // tx should revert
       await expect(
         hhBlankHoodie
-          .connect(addr1)
+          .connect(user)
           .mint(1, { value: ethers.utils.parseEther("0.08") })
       ).to.be.revertedWith("Insufficient funds!");
-      await expect(hhBlankHoodie.connect(addr1).mint(2)).to.be.revertedWith(
+      await expect(hhBlankHoodie.connect(user).mint(2)).to.be.revertedWith(
         "Insufficient funds!"
       );
 
       // balance should remain 0
-      expect(await hhBlankHoodie.balanceOf(addr1.address)).to.equal(0);
+      expect(await hhBlankHoodie.balanceOf(user.address)).to.equal(0);
     });
 
     it("should revert if max supply is exceeded", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
-
       // mint all NFTs so that max supply is reached
       await hhBlankHoodie.mint(500, { value: ethers.utils.parseEther("50") });
 
       // should revert if tries to mint another
       await expect(
         hhBlankHoodie
-          .connect(addr1)
+          .connect(user)
           .mint(1, { value: ethers.utils.parseEther("0.1") })
       ).to.be.revertedWith("Max supply exceeded!");
     });
   });
 
   describe("airDrop() functionality", () => {
-    it("owner should be able to mint anyone for free", async () => {
-      const { addr1, addr2, hhBlankHoodie } = await loadFixture(
-        deployContractFixture
-      );
+    let owner, user;
+    let hhBlankHoodie = null;
 
+    beforeEach(async () => {
+      [owner, user] = await ethers.getSigners();
+      const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
+      hhBlankHoodie = await BlankHoodie.deploy();
+
+      await hhBlankHoodie.setPaused(false);
+      await hhBlankHoodie.deployed();
+    });
+
+    it("owner should be able to mint anyone for free", async () => {
       // airdrop to some address
-      await hhBlankHoodie.airDrop(addr1.address, 1);
-      await hhBlankHoodie.airDrop(addr2.address, 3);
+      await hhBlankHoodie.airDrop(owner.address, 1);
+      await hhBlankHoodie.airDrop(user.address, 3);
 
       // check balances
-      expect(await hhBlankHoodie.balanceOf(addr1.address)).to.equal(1);
-      expect(await hhBlankHoodie.balanceOf(addr2.address)).to.equal(3);
+      expect(await hhBlankHoodie.balanceOf(owner.address)).to.equal(1);
+      expect(await hhBlankHoodie.balanceOf(user.address)).to.equal(3);
     });
 
     it("should revert if caller not the owner", async () => {
-      const { addr1, addr2, hhBlankHoodie } = await loadFixture(
-        deployContractFixture
-      );
-
       // tx should revert if caller not owner
       await expect(
-        hhBlankHoodie.connect(addr1).airDrop(addr2.address, 2)
+        hhBlankHoodie.connect(user).airDrop(owner.address, 2)
       ).to.be.revertedWith("Ownable: caller is not the owner");
     });
 
     it("should revert if max supply is exceeded", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
-
       // mint all NFTs to reach max supply
       await hhBlankHoodie.mint(500, { value: ethers.utils.parseEther("50") });
       // should revert if mint another one
-      await expect(hhBlankHoodie.airDrop(addr1.address, 4)).to.be.revertedWith(
+      await expect(hhBlankHoodie.airDrop(user.address, 4)).to.be.revertedWith(
         "Max supply exceeded!"
       );
     });
   });
 
   describe("tokenURI() functionality", () => {
-    it("should return valid URI for minted tokens", async () => {
-      const { hhBlankHoodie } = await loadFixture(deployContractFixture);
+    let owner, user;
+    let hhBlankHoodie = null;
 
+    beforeEach(async () => {
+      [owner, user] = await ethers.getSigners();
+      const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
+      hhBlankHoodie = await BlankHoodie.deploy();
+
+      await hhBlankHoodie.setPaused(false);
+      await hhBlankHoodie.deployed();
+    });
+
+    it("should return valid URI for minted tokens", async () => {
       // mint 2 NFTs and set base URI
       await hhBlankHoodie.mint(2, { value: ethers.utils.parseEther("0.2") });
 
@@ -120,8 +123,6 @@ describe("BlankHoodie contract", () => {
     });
 
     it("should revert if calls tokenURI() for not minted token", async () => {
-      const { hhBlankHoodie } = await loadFixture(deployContractFixture);
-
       await expect(hhBlankHoodie.tokenURI(0)).to.be.revertedWith(
         "ERC721Metadata: URI query for nonexistent token"
       );
@@ -129,9 +130,19 @@ describe("BlankHoodie contract", () => {
   });
 
   describe("setBaseURI() functionality", () => {
-    it("owner should be able to update base URI anytime", async () => {
-      const { owner, hhBlankHoodie } = await loadFixture(deployContractFixture);
+    let owner, user;
+    let hhBlankHoodie = null;
 
+    beforeEach(async () => {
+      [owner, user] = await ethers.getSigners();
+      const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
+      hhBlankHoodie = await BlankHoodie.deploy();
+
+      await hhBlankHoodie.setPaused(false);
+      await hhBlankHoodie.deployed();
+    });
+
+    it("owner should be able to update base URI anytime", async () => {
       // update URI
       await hhBlankHoodie.setBaseURI("new URI");
 
@@ -141,18 +152,26 @@ describe("BlankHoodie contract", () => {
     });
 
     it("should revert if caller not the owner", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
-
       expect(
-        hhBlankHoodie.connect(addr1).setBaseURI("some-new-URI")
+        hhBlankHoodie.connect(user).setBaseURI("some-new-URI")
       ).to.be.revertedWith("Ownable: Caller not the owner");
     });
   });
 
   describe("setPaused() functionality", () => {
-    it("pause/unpause minting", async () => {
-      const { hhBlankHoodie } = await loadFixture(deployContractFixture);
+    let owner, user;
+    let hhBlankHoodie = null;
 
+    beforeEach(async () => {
+      [owner, user] = await ethers.getSigners();
+      const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
+      hhBlankHoodie = await BlankHoodie.deploy();
+
+      await hhBlankHoodie.setPaused(false);
+      await hhBlankHoodie.deployed();
+    });
+
+    it("pause/unpause minting", async () => {
       // pause
       await hhBlankHoodie.setPaused(true);
       // should revert when minting
@@ -169,52 +188,66 @@ describe("BlankHoodie contract", () => {
     });
 
     it("should revert if caller not the owner", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
-
       await expect(
-        hhBlankHoodie.connect(addr1).setPaused(true)
+        hhBlankHoodie.connect(user).setPaused(true)
       ).to.be.revertedWith("Ownable: caller is not the owner");
     });
   });
 
   describe("setCost() functionality", () => {
-    it("should change cost accordingly if called by owner", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
+    let owner, user;
+    let hhBlankHoodie = null;
 
+    beforeEach(async () => {
+      [owner, user] = await ethers.getSigners();
+      const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
+      hhBlankHoodie = await BlankHoodie.deploy();
+
+      await hhBlankHoodie.setPaused(false);
+      await hhBlankHoodie.deployed();
+    });
+
+    it("should change cost accordingly if called by owner", async () => {
       // change cost
       await hhBlankHoodie.setCost(ethers.utils.parseEther("1"));
       // minting on previous price should revert
       await expect(
         hhBlankHoodie
-          .connect(addr1)
+          .connect(user)
           .mint(1, { value: ethers.utils.parseEther("0.1") })
       ).to.be.revertedWith("Insufficient funds!");
       // minting on new price should work
       await expect(
         hhBlankHoodie
-          .connect(addr1)
+          .connect(user)
           .mint(1, { value: ethers.utils.parseEther("1") })
       ).to.not.be.reverted;
     });
 
     it("should revert if caller not the owner", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
-
       await expect(
-        hhBlankHoodie.connect(addr1).setCost(ethers.utils.parseEther("1"))
+        hhBlankHoodie.connect(user).setCost(ethers.utils.parseEther("1"))
       ).to.be.revertedWith("Ownable: caller is not the owner");
     });
   });
 
   describe("withdraw() functionality", () => {
-    it("should transfer contract balance to owner if called by owner", async () => {
-      const { owner, addr1, hhBlankHoodie } = await loadFixture(
-        deployContractFixture
-      );
+    let owner, user;
+    let hhBlankHoodie = null;
 
+    beforeEach(async () => {
+      [owner, user] = await ethers.getSigners();
+      const BlankHoodie = await ethers.getContractFactory("BlankHoodie");
+      hhBlankHoodie = await BlankHoodie.deploy();
+
+      await hhBlankHoodie.setPaused(false);
+      await hhBlankHoodie.deployed();
+    });
+
+    it("should transfer contract balance to owner if called by owner", async () => {
       // mint NFTs to send ether to contract
       await hhBlankHoodie
-        .connect(addr1)
+        .connect(user)
         .mint(5, { value: ethers.utils.parseEther("0.5") });
 
       // withdraw money and check if ether is transfered to owner
@@ -225,9 +258,7 @@ describe("BlankHoodie contract", () => {
     });
 
     it("should revert if caller not the owner", async () => {
-      const { addr1, hhBlankHoodie } = await loadFixture(deployContractFixture);
-
-      await expect(hhBlankHoodie.connect(addr1).withdraw()).to.be.revertedWith(
+      await expect(hhBlankHoodie.connect(user).withdraw()).to.be.revertedWith(
         "Ownable: caller is not the owner"
       );
     });
