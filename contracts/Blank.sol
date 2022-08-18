2513176d2c11e07f4bbc59b67eedcc8477205ba6
ldzoid <lucijandrnasin1@gmail.com>
2022-08-18 13:21:16 +0200

    finish erc20 smart contract

---
+++ contracts/Blank.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import './BlankDependencies/ERC20Capped.sol';
+
+contract Blank is ERC20Capped {
+
+    constructor() ERC20Capped(1000000) ERC20('Blank', 'BLANK') {
+        _mint(msg.sender, 100**18);
+    }
+
+}
