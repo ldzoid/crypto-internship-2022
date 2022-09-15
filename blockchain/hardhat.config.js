d8db0006f22b2a9da59530e9af7756e795108854
ldzoid <lucijandrnasin1@gmail.com>
2022-09-15 16:46:16 +0200

    add gas reporter with USD values

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -26,5 +27,12 @@ module.exports = {
     apiKey: {
       goerli: ETHERSCAN_API_KEY,
     },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
